191343b1b8295531cc59d9c63a319fca89f5f786
lukefrederick <lfrederick220@gmail.com>
2025-07-18 20:53:12 -0500

    Submission

---
+++ arrays4.js
@@ -0,0 +1,22 @@
+// Task 1
+let allScores = [
+    [67, 83, 89],
+    [73, 89, 94],
+    [75, 84, 96]
+];
+
+// Task 2
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"],
+];
+
+let contactsList = [
+    ["Luke", "luke@gmail.com", "636-111-1111"],
+    ["Alex", "alex@gmail.com", "314-222-1111"],
+];
+
+console.log(allScores);
+console.log(theaterSeating);
+console.log(contactsList);
